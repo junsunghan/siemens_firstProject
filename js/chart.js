a0384d8ce3a17ae3668c009837f522bd9ba8766a
junsunghan <hjs8213073@gmail.com>
2023-03-10 14:40:50 +0900

    주석까지 완료

---
+++ js/chart.js
@@ -0,0 +1,106 @@
+let date = new Date(); //Date 객체 생성
+let movieN = new Array(); //영화 이름 담는 전역변수
+let movieC = new Array(); //해당일 관객 수 담는 전역변수
+
+//달력에 Date객체를 통해 오늘날짜 넣기
+let today_date =
+  date.getFullYear() +
+  "-" +
+  ("0" + (1 + date.getMonth())).slice(-2) +
+  "-" +
+  ("0" + date.getDate()).slice(-2);
+
+$("#cal").attr("value", today_date); //달력에 오늘 날짜 입력
+
+//달력에 날짜 지정했을 때 박스오피스 출력하는 이벤트
+$("#cal").change(function () {
+  call_chart();
+});
+
+//박스오피스와 차트를 불러오는 함수
+let call_chart = () => {
+  let sel_date = $("#cal").val(); //선택한 날짜 불러오기
+
+  if (sel_date < "20000101") {
+    alert("2000년대 이후로 입력해주세요");
+  } else if (sel_date >= today_date) {
+    alert("당일보다 하루 전날 까지 출력됩니다");
+  } else {
+    let taget_day =
+      sel_date.substring(0, 4) +
+      sel_date.substring(5, 7) +
+      sel_date.substring(8, 10);
+    date_check(taget_day); // 제어문 통과하면 박스오피스 불러오는 함수 호출
+  }
+};
+
+//박스오피스 불러오는 함수
+let date_check = (taget_day) => {
+  $.ajax({
+    url: "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json",
+    data: {
+      key: "f5eef3421c602c6cb7ea224104795888",
+      targetDt: taget_day,
+    },
+    success: function (result) {
+      console.log(result);
+      let movieList = result.boxOfficeResult.dailyBoxOfficeList; //영화 이름
+      let movie_list_cnt = result.boxOfficeResult.dailyBoxOfficeList; //영화 관객수
+      for (let i = 0; i < movieList.length; i++) {
+        //전역변수로 설정한 movieN과 movieC에 영화이름과 관객수를 저장해놓음
+        movieN.push(movieList[i].movieNm);
+        movieC.push(movie_list_cnt[i].audiCnt);
+      }
+    },
+    error: function () {
+      alert("error");
+    },
+    complete: function () {
+      boxoffice_yesterday_chart(); //콜백함수 대신 jQuery ajax를 통해 제어
+    },
+  });
+};
+
+//구글차트 그리기API - 박스오피스 순위
+let boxoffice_yesterday_chart = () => {
+  // Load the Visualization API and the corechart package.
+  google.charts.load("current", { packages: ["corechart"] });
+
+  // Set a callback to run when the Google Visualization API is loaded.
+  google.charts.setOnLoadCallback(drawChart);
+
+  // Callback that creates and populates a data table,
+  // instantiates the pie chart, passes in the data and
+  // draws it.
+
+  function drawChart() {
+    // Create the data table.
+    let data = new google.visualization.DataTable();
+    data.addColumn("string", "영화제목");
+    data.addColumn("number", "관객수");
+    data.addRows([
+      [movieN[0], parseInt(movieC[0])],
+      [movieN[1], parseInt(movieC[1])],
+      [movieN[2], parseInt(movieC[2])],
+      [movieN[3], parseInt(movieC[3])],
+      [movieN[4], parseInt(movieC[4])],
+    ]);
+
+    // Set chart options
+    let options = {
+      title: "박스오피스",
+      width: 500,
+      height: 400,
+    };
+
+    // Instantiate and draw our chart, passing in some options.
+    let chart = new google.visualization.BarChart(
+      document.getElementById("chart_div")
+    );
+    chart.draw(data, options);
+
+    // 차트를 다 그리면 영화명과 관객수를 담은 배열을 초기화 해준다.
+    movieN = [];
+    movieC = [];
+  }
+};
---
+++ js/map.js
@@ -0,0 +1,37 @@
+create_map(); //첫 화면 출력시 호출
+
+//지도 만드는 함수
+  function create_map(  //인수 없을 시 기본 좌표
+    a = "37.4561636344619",
+    b = "126.71133809339524",
+    c = "준성집"
+  ) {
+
+    // 카카오 지도 API 시작
+    var container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
+    var options = {
+      //지도를 생성할 때 필요한 기본 옵션
+      center: new kakao.maps.LatLng(a, b), //지도의 중심좌표.
+      level: 3, //지도의 레벨(확대, 축소 정도)
+    };
+    var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+    // 카카오 지도 API 끝
+
+    var marker = new kakao.maps.Marker({
+      // 지도 중심좌표에 마커를 생성합니다
+      position: map.getCenter(),
+    });
+
+    // 인포윈도우로 장소에 대한 설명을 표시합니다
+    var infowindow = new kakao.maps.InfoWindow({
+      content:
+        `<div style="width:150px;text-align:center;padding:6px 0;">${c}</div>`,
+    });
+    infowindow.open(map, marker);
+
+    // 지도에 마커를 표시합니다
+    marker.setMap(map);
+
+    // 지도 상단에 장소명 변경 이벤트
+    $("#map_name").html(c);
+  }
